3c21837851b2d20d72ce0324baf67cdb66a22781
piaoroumi <252132729@qq.com>
2022-01-14 13:15:04 +0800

    jni learning

--- mynew.c
+++ mynew.c
@@ -1,3 +1,9 @@
-#include "stdio.h"
-int main(void *arg void **arg[])
-{}
+#include <jni.h>
+#include <stdio.h>
+#include "HelloJNI.h/HelloJNI.h"
+
+// Implementation of native method sayHello() of HelloJNI class
+JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
+   printf("Hello World!\n");
+   return;
+}
